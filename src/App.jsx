5ae0655898bed280ffc8905fb27e40217b224f6c
amie-musa <106128318+amie-musa@users.noreply.github.com>
2026-06-07 00:15:02 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div className="container">
+      <h1>RSVP Test</h1>
+
+      <input placeholder="Enter your name" />
+
+      <select>
+        <option>1 Guest</option>
+        <option>2 Guests</option>
+        <option>3 Guests</option>
+      </select>
+
+      <button>Submit</button>
+    </div>
+  );
+}
