71ea424f9596e49fc3754057d47076529f15731a
amie-musa <amielyn.musa@gmail.com>
2026-06-06 23:59:29 -0400

    initial rsvp test

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>RSVP Test</h1>
+
+      <input
+        type="text"
+        placeholder="Enter your name"
+      />
+
+      <button>
+        Submit
+      </button>
+    </div>
+  );
+}
